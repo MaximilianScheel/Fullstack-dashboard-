75566e099fe6250390ddfc87ae845ef98c730207
Maximilian Scheel <m.scheel@dotsource.de>
2024-01-31 14:25:22 +0100

    Impl. Login

---
+++ src/components/layout/current-user.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Popover, Button } from 'antd'
+import CustomAvatar from '../custom-avatar'
+import { useGetIdentity } from '@refinedev/core'
+
+import type { User } from '@/graphql/schema.types'
+
+const CurrentUser = () => {
+  const { data: user } = useGetIdentity<User>()
+
+  const content = ( 
+  <div style={{
+    display: 'flex',
+    flexDirection: 'column',
+  }}>
+    
+  </div>
+  )
+
+  return (
+    <>
+    <Popover
+      placement='bottomRight'
+      trigger='click'
+      overlayInnerStyle={{padding: 0}}
+      overlayStyle={{zIndex: 1000}}
+    >
+      <CustomAvatar 
+        name={user?.name}
+        src={user?.avatarUrl}
+        size='default'
+        style={{cursor: 'pointer'}}
+      />
+    </Popover>
+    </>
+  )
+}
+
+export default CurrentUser
